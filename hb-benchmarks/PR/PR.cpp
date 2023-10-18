d06e9882179933c407f6475f9445e6e837540ab7
Yian Su <yiansu2018@u.northwestern.edu>
2023-10-18 00:23:22 -0500

    PR: integrate heartbeat compilation and evaluation pipeline

--- hb-benchmarks/PR/PR.cpp
+++ hb-benchmarks/PR/PR.cpp
@@ -8,6 +8,16 @@
 #include <pybind11/numpy.h>
 namespace py = pybind11;
 #endif
+
+#if defined(USE_BASELINE) || defined(USE_FORKJOIN)
+#include <functional>
+#include <taskparts/benchmark.hpp>
+#elif defined(USE_OPENMP)
+#include "utility.hpp"
+#elif defined(USE_HB_COMPILER) || defined(USE_HB_MANUAL)
+#include "loop_handler.hpp"
+#endif
+
 Graph edges;
 double  * __restrict old_rank;
 double  * __restrict new_rank;
@@ -101,9 +111,150 @@ void operator() (NodeID v)
     new_rank[v] = ((float) 0) ;
   };
 };
+
+#if defined(USE_BASELINE) || defined(TEST_CORRECTNESS)
+void PR_baseline(Graph &g) {
+  // for ( int i = (0) ; i < (20) ; i++ )
+  // {
+  //   ligra::parallel_for_lambda((int)0, (int)builtin_getVertices(edges) , [&] (int vertexsetapply_iter) {
+  //     computeContrib()(vertexsetapply_iter);
+  //   });;
+  //   edgeset_apply_pull_parallel6(edges, updateEdge()); 
+  //   ligra::parallel_for_lambda((int)0, (int)builtin_getVertices(edges) , [&] (int vertexsetapply_iter) {
+  //     updateVertex()(vertexsetapply_iter);
+  //   });;
+  // }
+  for ( int i = (0) ; i < (20) ; i++ )
+  {
+    for (uint64_t nodeID = 0; nodeID < builtin_getVertices(edges); nodeID++) {
+      computeContrib()(nodeID);
+    }
+
+    for (uint64_t d = 0; d < g.num_nodes(); d++) {
+      for (uint64_t i = g.get_in_neighbors_begin_index_(d); i < g.get_in_neighbors_end_index_(d); i++) {
+        updateEdge()(g.get_in_neighbors_()[i], d);
+      }
+    }
+
+    for (uint64_t nodeID = 0; nodeID < builtin_getVertices(edges); nodeID++) {
+      updateVertex()(nodeID);
+    }
+  }
+}
+
+#if defined(TEST_CORRECTNESS)
+void test_correctness() {
+  // save previous output
+  double  * __restrict old_rank_output = new double [ builtin_getVertices(edges) ];
+  for (uint64_t i = 0; i < builtin_getVertices(edges); i++) {
+    old_rank_output[i] = old_rank[i];
+  }
+  ligra::parallel_for_lambda((int)0, (int)builtin_getVertices(edges) , [&] (int vertexsetapply_iter) {
+    reset()(vertexsetapply_iter);
+  });;
+
+  // run serial and generate output
+  PR_baseline(edges);
+
+  // compare previous ourput with serial-version output
+  uint64_t num_diffs = 0;
+  double epsilon = 0.01;
+  for (uint64_t i = 0; i < builtin_getVertices(edges); i++) {
+    auto diff = std::abs(old_rank_output[i] - old_rank[i]);
+    if (diff > epsilon) {
+      printf("diff=%f old_rank_output[i]=%f old_rank[i]=%f at i=%lu\n", diff, old_rank_output[i], old_rank[i], i);
+      num_diffs++;
+    }
+  }
+  if (num_diffs > 0) {
+    printf("\033[0;31mINCORRECT!\033[0m");
+    printf("  num_diffs = %lu\n", num_diffs);
+  } else {
+    printf("\033[0;32mCORRECT!\033[0m\n");
+  }
+}
+#endif
+#endif
+
+#if defined(USE_OPENMP)
+void PR_openmp(Graph &g) {
+  for ( int i = (0) ; i < (20) ; i++ )
+  {
+    #pragma omp parallel for schedule(static)
+    for (uint64_t nodeID = 0; nodeID < builtin_getVertices(edges); nodeID++) {
+      computeContrib()(nodeID);
+    }
+
+    #if defined(OMP_SCHEDULE_STATIC)
+      #pragma omp parallel for schedule(static)
+    #elif defined(OMP_SCHEDULE_DYNAMIC)
+      #pragma omp parallel for schedule(dynamic)
+    #elif defined(OMP_SCHEDULE_GUIDED)
+      #pragma omp parallel for schedule(guided)
+    #endif
+    for (uint64_t d = 0; d < g.num_nodes(); d++) {
+      for (uint64_t i = g.get_in_neighbors_begin_index_(d); i < g.get_in_neighbors_end_index_(d); i++) {
+        updateEdge()(g.get_in_neighbors_()[i], d);
+      }
+    }
+
+    #pragma omp parallel for schedule(static)
+    for (uint64_t nodeID = 0; nodeID < builtin_getVertices(edges); nodeID++) {
+      updateVertex()(nodeID);
+    }
+  }
+}
+
+#elif defined(USE_HB_COMPILER)
+#if defined(RUN_HEARTBEAT)
+  bool run_heartbeat = true;
+#else
+  bool run_heartbeat = false;
+#endif
+
+void HEARTBEAT_loop0(uint64_t maxIter) {
+  for (uint64_t nodeID = 0; nodeID < maxIter; nodeID++) {
+    computeContrib()(nodeID);
+  }
+}
+
+void HEARTBEAT_loop2(
+  uint64_t startIter,
+  uint64_t maxIter,
+  Graph &g,
+  uint64_t d
+) {
+  for (uint64_t i = startIter; i < maxIter; i++) {
+    updateEdge()(g.get_in_neighbors_()[i], d);
+  }
+}
+
+void HEARTBEAT_loop1(Graph &g, uint64_t maxIter) {
+  for (uint64_t d = 0; d < maxIter; d++) {
+    HEARTBEAT_loop2(g.get_in_neighbors_begin_index_(d), g.get_in_neighbors_end_index_(d), g, d);
+  }
+}
+
+void HEARTBEAT_loop3(uint64_t maxIter) {
+  for (uint64_t nodeID = 0; nodeID < maxIter; nodeID++) {
+    updateVertex()(nodeID);
+  }
+}
+
+void PR_hbc(Graph &g) {
+  for ( int i = (0) ; i < (20) ; i++ )
+  {
+    HEARTBEAT_loop0(builtin_getVertices(edges));
+    HEARTBEAT_loop1(g, g.num_nodes());
+    HEARTBEAT_loop3(builtin_getVertices(edges));
+  }
+}
+#endif
+
+
 int main(int argc, char * argv[])
 {
-  edges = builtin_loadEdgesFromFile ( argv_safe((1) , argv, argc)) ;
+  edges = builtin_loadEdgesFromFile ("USAroad.el") ;
   old_rank = new double [ builtin_getVertices(edges) ];
   new_rank = new double [ builtin_getVertices(edges) ];
   out_degree = new int [ builtin_getVertices(edges) ];
@@ -127,27 +278,46 @@ int main(int argc, char * argv[])
   ligra::parallel_for_lambda((int)0, (int)builtin_getVertices(edges) , [&] (int vertexsetapply_iter) {
     error_generated_vector_op_apply_func_5()(vertexsetapply_iter);
   });;
-  for ( int trail = (0) ; trail < (10) ; trail++ )
-  {
-    startTimer() ;
+  // for ( int trail = (0) ; trail < (10) ; trail++ )
+  // {
+    // startTimer() ;
     ligra::parallel_for_lambda((int)0, (int)builtin_getVertices(edges) , [&] (int vertexsetapply_iter) {
       reset()(vertexsetapply_iter);
     });;
-    for ( int i = (0) ; i < (20) ; i++ )
-    {
-      ligra::parallel_for_lambda((int)0, (int)builtin_getVertices(edges) , [&] (int vertexsetapply_iter) {
-        computeContrib()(vertexsetapply_iter);
-      });;
-      edgeset_apply_pull_parallel6(edges, updateEdge()); 
-      ligra::parallel_for_lambda((int)0, (int)builtin_getVertices(edges) , [&] (int vertexsetapply_iter) {
-        updateVertex()(vertexsetapply_iter);
-      });;
-    }
-    double elapsed_time = stopTimer() ;
-    std::cout << "elapsed time: "<< std::endl;
-    std::cout << elapsed_time<< std::endl;
+// ####################
+#if defined(USE_BASELINE) || defined(USE_FORKJOIN)
+  taskparts::benchmark_nativeforkjoin([&] (auto sched) {
+#elif defined(USE_OPENMP)
+  utility::run([&] {
+#elif defined(USE_HB_COMPILER) || defined(USE_HB_MANUAL)
+  run_bench([&] {
+#endif
+
+#if defined(USE_BASELINE)
+    PR_baseline(edges);
+#elif defined(USE_OPENMP)
+    PR_openmp(edges);
+#elif defined(USE_HB_COMPILER)
+    PR_hbc(edges);
+#endif
+
+#if defined(TEST_CORRECTNESS)
+    test_correctness();
+#endif
+
+#if defined(USE_BASELINE) || defined(USE_FORKJOIN)
+  }, [&] (auto sched) {
+  }, [&] (auto sched) {
+  });
+#else
+  }, [&] {}, [&] {});
+#endif
+// ####################
+    // double elapsed_time = stopTimer() ;
+    // std::cout << "elapsed time: "<< std::endl;
+    // std::cout << elapsed_time<< std::endl;
   }
-};
+// };
 #ifdef GEN_PYBIND_WRAPPERS
 PYBIND11_MODULE(, m) {
 }
